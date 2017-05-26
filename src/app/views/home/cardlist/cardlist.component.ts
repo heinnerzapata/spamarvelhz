c49cd664290d448fab55a1ee8645c00f15c8bdb7
Heinner Zapata Escarraga <heinner.zapata@SINCO.NET>
2017-05-25 19:30:23 -0500

    service test

--- src/app/views/home/cardlist/cardlist.component.ts
+++ src/app/views/home/cardlist/cardlist.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { Character } from './../../../models/character';
+import { MarvelApiService } from './../../../services/marvelapi.service';
 
 @Component({
     selector : 'cardlist-component',
     templateUrl : './cardlist.component.html',
-    styleUrls: ['./cardlist.component.css']
+    styleUrls: ['./cardlist.component.css'],
+    providers:[MarvelApiService]
 })
 
 export class CardListComponent{
 
+  constructor(private marvelApiService: MarvelApiService) {
+
+    this.consultar();
+
+  }
+
+  public characters = [];
+
+  consultar() {
+    this.marvelApiService.getCharacters().subscribe(
+      (data) => this.characters = data.data.results
+    );
+  }
+
+  /*
   public characters : Array<Character> = [
 
     new Character(1,'Heinner','heinner desc','jpg','http://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf'),
@@ -17,6 +34,8 @@ export class CardListComponent{
     new Character(4,'Juan','Jaun desc','jpg','http://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf'),
 
   ];
+
+  */
 
   //new Character(1,'Heinner','heinner desc','jpg','http://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf');
 
@@ -27,8 +46,5 @@ export class CardListComponent{
 
   //https://gateway.marvel.com/v1/public/characters?nameStartsWith=wolv&limit=50&ts=1&apikey=3ea1cd1673639f6a528e957b1db5939b&hash=cfa3f7936bc2ccd074e8876351bcffa3
 
-  constructor(){
-
-  }
 
 }
